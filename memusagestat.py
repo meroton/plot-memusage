6227c573c7180082254e47d78023dbbcbdb5104d
Nils Wireklint <nils@meroton.com>
2023-09-21 11:31:01 +0200

    Add command line flags

--- memusagestat.py
+++ memusagestat.py
@@ -1,32 +1,37 @@
 import sys
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import argparse
 import struct
 
 from dataclasses import dataclass
-from typing import List, Tuple, TypeVar
+from typing import Any, Callable, List, Optional, Tuple, TypeVar
 
 FORMAT = "QQII"  # u64, u64, u32, u32
 SIZE = struct.calcsize(FORMAT)
 assert SIZE == 8 + 8 + 4 + 4
 
+
 @dataclass
 class ParsedEntry:
-    heap: int # U64
-    stack: int # U64
-    time_low: int  # U32
-    time_high: int # U32
+    heap: int  # u64
+    stack: int  # u64
+    time_low: int  # u32
+    time_high: int  # u32
+
 
 @dataclass
 class Entry:
-    heap: int  # U64
-    stack: int  # U64
-    time: int  # U64
+    heap: int  # u64
+    stack: int  # u64
+    time: int  # u64
+
 
 def parse(slice: bytes) -> ParsedEntry:
     parts = struct.unpack(FORMAT, slice)
     heap, stack, low, high = parts
     return ParsedEntry(heap, stack, low, high)
+
 
 def time(entry: ParsedEntry, start=0, scale=1) -> Entry:
     """Compute the time in [UNIT]
@@ -39,40 +44,80 @@ def time(entry: ParsedEntry, start=0, scale=1) -> Entry:
     time /= scale
     return Entry(entry.heap, entry.stack, time)
 
-T = List[int]
-def plot(stacks: T, heaps: T, times: T, *, image: str):
+
+def plot(
+    *,
+    stacks: List[int],
+    heaps: List[int],
+    xs: List[int],
+    image: str,
+    total: bool,
+):
     fix, ax = plt.subplots()
-    # ax.plot(range(len(times)), heaps)
-    ax.plot(times, heaps, linestyle=None, label="heap")
+    ax.plot(xs, heaps, linestyle=None, label="heap", color="red")
+    ax.plot(xs, stacks, linestyle=None, label="stack", color="green")
+
+    if total:
+        totals = [0] * len(xs)
+        for i in range(len(xs)):
+            totals[i] = stacks[i] + heaps[i]
+        ax.plot(xs, totals, linestyle=None, label="total", color="black")
+
     plt.savefig(image)
     print(f"Saved {image}")
 
 
-def main(datafile: str, imagefile: str):
-    data = open(sys.argv[1], 'rb').read()
+def arguments(args: List[str]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-T",
+        "--total",
+        help="Also draw graph for total memory consumption",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        help="Generate output linear to time (default is linear to number of function calls)",
+        action="store_true",
+    )
+    parser.add_argument("datafile", help="Datafile from `memusage`")
+    parser.add_argument("imagefile", help="Output filename for the image")
+
+    return parser.parse_args(args)
+
+
+def main(
+    datafile: str,
+    imagefile: str,
+    *,
+    xtransformer: Callable[[List[int]], List[int]],
+    total: bool,
+):
+    data = open(sys.argv[1], "rb").read()
     count = int(len(data) / SIZE) - 2
 
     stacks = [0] * count
     heaps = [0] * count
     times = [0] * count
-    entries = [None] * count  # type: List[Entry]
+    entries: List[Entry] = [Entry(0, 0, 0)] * count
 
     headers = [
         parse(data[0:SIZE]),
-        parse(data[SIZE:2*SIZE]),
+        parse(data[SIZE : 2 * SIZE]),
     ]
 
-    first = time(parse(data[2*SIZE: 3*SIZE]))
+    first = time(parse(data[2 * SIZE : 3 * SIZE]))
 
     for i in range(count):
-        start = 2*SIZE + SIZE*i
-        end = 2*SIZE + SIZE*i + SIZE
+        start = 2 * SIZE + SIZE * i
+        end = 2 * SIZE + SIZE * i + SIZE
         entry = time(parse(data[start:end]), start=first.time, scale=1e10)
         entries[i] = entry
 
     # The data files contains buffered entries,
     # so the time is usually increasing but a different chunk can jump backward.
-    entries = sorted(entries, key = lambda e: e.time)
+    entries = sorted(entries, key=lambda e: e.time)
 
     for i, entry in enumerate(entries):
         if i > 0:
@@ -85,9 +130,25 @@ def main(datafile: str, imagefile: str):
         heaps[i] = entry.heap
         times[i] = entry.time
 
+    plot(
+        stacks=stacks, heaps=heaps, xs=xtransformer(times), image=imagefile, total=total
+    )
 
-    plot(stacks, heaps, times, image = imagefile)
 
+if __name__ == "__main__":
+    args = arguments(sys.argv[1:])
+    print(args)
 
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    xtransformer = {
+        True: lambda x: x,
+        False: lambda x: range(len(x)),
+    }[args.time]
+
+    xtransformer2 = lambda x: x
+
+    main(
+        args.datafile,
+        args.imagefile,
+        xtransformer=xtransformer,
+        total=args.total,
+    )
